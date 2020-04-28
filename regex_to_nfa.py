58e831d0e4aa2e682a6fde2d2d72393dac669fbc
Mihailo Stojanovic <mihailo.stojanovic@rt-rk.com>
2020-04-28 12:08:36 +0200

    Finish implementation of regex to NFA algorithm
    Added union operator transformation. Put the common code which offsets
    the outgoing states of the graph into separate function. Added some
    helper funtions.
    
            * regex_to_nfa.py (transform_union): New function.
            (visualize_graph): New function.
            (print_matrix): New function.
            (offset_outgoing_states): New function.

--- regex_to_nfa.py
+++ regex_to_nfa.py
@@ -1,5 +1,7 @@
 from regex_parser import parse
 from regex_lexer import tokenize_regex
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # transformation of regex ASTs to non-deterministic finite automata (NFAs) using the McNaughton-Yamada-Thompson
 # algorithm
@@ -14,8 +16,36 @@ from regex_lexer import tokenize_regex
 # thus, the transition matrix is sparse so I decided to leave out the empty transitions and implement every state as
 # a list of tuples whose first element is the input symbol of the transition, while the other elements (one or two) are
 # the outgoing states
+# first row of the matrix always represents the starting state of the DFA, while the last row represents the
+# accepting state
 #
 # [1] The Dragon Book, 2nd Ed, p. 161
+
+
+# helper function which visualizes the graph
+def visualize_graph(mat):
+    graph = nx.DiGraph()
+
+    for i in range(0, len(mat)):
+        for in_sym in mat[i]:
+            if in_sym[0] == 'eps':
+                if len(in_sym) == 1:
+                    continue
+                elif len(in_sym) == 3:
+                    graph.add_edge(i, in_sym[2], sym='eps')
+                graph.add_edge(i, in_sym[1], sym='eps')
+            else:
+                graph.add_edge(i, in_sym[1], sym=in_sym[0])
+
+    nx.draw(graph, with_labels=True)
+    plt.show()
+
+
+# helper function which prints the matrix
+def print_matrix(mat):
+    for state in mat:
+        print(state)
+    print()
 
 
 # transform the simplest one-character expression (eg. 'a') to DFA
@@ -27,36 +57,9 @@ def transform_simple_expression(expr):
     ]
 
 
-# create Kleene closure DFA
-def transform_kleene(mat):
-    # increment all the fields by one, as we are inserting starting state as the first row of the matrix
+# helper function which offsets the outgoing states of the matrix by the desired value
+def offset_outgoing_states(mat, offset):
     for state in mat:
-        for in_sym in state:
-            if in_sym[0] == 'eps':
-                if len(in_sym) == 1:
-                    continue
-                elif len(in_sym) == 3:
-                    in_sym[2] += 1
-                in_sym[1] += 1
-            else:
-                in_sym[1] += 1
-
-    # insert new starting and accepting states
-    mat.insert(0, [['eps', 1, len(mat) + 1]])
-    mat.append([['eps']])
-
-    # correct the old accepting state
-    mat[len(mat) - 1][0].append(1)
-    mat[len(mat) - 1][0].append(len(mat) - 1)
-
-    return mat
-
-
-def transform_concat(left, right):
-    # offset the indexes in the right DFA matrix, as we are removing the first (starting) state from it
-    # and appending it to the left DFA matrix
-    offset = len(left) - 1
-    for state in right:
         for in_sym in state:
             if in_sym[0] == 'eps':
                 if len(in_sym) == 1:
@@ -66,6 +69,30 @@ def transform_concat(left, right):
                 in_sym[1] += offset
             else:
                 in_sym[1] += offset
+
+    return mat
+
+
+# create Kleene closure DFA
+def transform_kleene(mat):
+    # offset all the outgoing states by one, as we are inserting starting state as the first row of the matrix
+    mat = offset_outgoing_states(mat, 1)
+
+    # insert new starting and accepting states
+    mat.insert(0, [['eps', 1, len(mat) + 1]])
+    mat.append([['eps']])
+
+    # correct the old accepting state
+    mat[len(mat) - 2][0].append(1)
+    mat[len(mat) - 2][0].append(len(mat) - 1)
+
+    return mat
+
+
+def transform_concat(left, right):
+    # offset the outgoing states in the right DFA matrix, as we are removing the first (starting) state from it
+    # and appending it to the left DFA matrix
+    right = offset_outgoing_states(right, len(left) - 1)
 
     # remove the starting state from the right DFA matrix
     start_state_right = right[0]
@@ -84,6 +111,32 @@ def transform_concat(left, right):
     return left
 
 
+def transform_union(left, right):
+    # offset the outgoing states of the left DFA matrix, as we are adding the starting state of the new DFA matrix
+    # at the beginning
+    left = offset_outgoing_states(left, 1)
+
+    # offset the outgoing states of the right DFA matrix, as we are appending it to the left DFA matrix (and also
+    # adding the starting state at the beginning of the left DFA matrix)
+    right = offset_outgoing_states(right, len(left) + 1)
+
+    start_state_right = len(left) + 1
+    new_accept_state_index = len(left) + len(right) + 1
+    # make the old accepting states point to the new accepting state
+    left[len(left) - 1][0].append(new_accept_state_index)
+    right[len(right) - 1][0].append(new_accept_state_index)
+
+    # append the right DFA matrix to the left DFA matrix
+    for state in right:
+        left.append(state)
+
+    # add the new starting and accepting state
+    left.insert(0, [['eps', 1, start_state_right]])
+    left.append([['eps']])
+
+    return left
+
+
 if __name__ == "__main__":
     token_list_a = tokenize_regex('a')
     token_list_b = tokenize_regex('b')
@@ -94,25 +147,10 @@ if __name__ == "__main__":
     a = transform_simple_expression(root_a)
     b = transform_simple_expression(root_b)
     c = transform_simple_expression(root_c)
-    kleene = transform_kleene(c)
-    for row in kleene:
-        print(row)
-    print('')
-    kleene = transform_concat(b, kleene)
-    for row in kleene:
-        print(row)
-    print('')
-    #concat = transform_concat(a, kleene)
-    #for row in concat[0]:
-    #    print(row)
-    #kleene = transform_kleene(c)[0]
-    #for row in kleene:
-    #    print(row)
-    #union = transform_union(a, b)
-    #for row in union[0]:
-    #    print(row)
-    #print('')
-    #union = transform_union(union, c)
-    #for row in union[0]:
-    #    print(row)
-
+    #visualize_graph(a)
+    kleene = transform_kleene(a)
+    print_matrix(kleene)
+    #visualize_graph(kleene)
+    kleene2 = transform_kleene(b)
+    kleene = transform_union(kleene, kleene2)
+    visualize_graph(kleene)
