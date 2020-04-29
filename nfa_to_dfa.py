9a597eb221e41d0c3ef813a225d7bda56410179c
Mihailo Stojanovic <mihailo.stojanovic@rt-rk.com>
2020-04-29 15:14:47 +0200

    Finish implementation of NFA to DFA transformation
            * nfa_to_dfa.py (print_dfa): New function.
            (compute_possible_in_syms): Ditto.
            (nfa_to_dfa): Ditto.

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -13,6 +13,22 @@ import copy
 # a new input symbol DFA is in the state which corresponds to the set of states that are reachable in the NFA, after
 # reading all previous input symbols
 
+# [1] The Dragon Book, 2nd edition, p. 165
+# [2] The Dragon Book, 2nd edition, p. 154
+
+
+# helper function which prints the DFA states and transition table
+def print_dfa(dstates, dtran, dfa_acc_states):
+    i = 0
+    for state in dstates:
+        print("State " + str(i))
+        print(state)
+        print(dtran[i])
+        if i in dfa_acc_states:
+            print("Accepting")
+        print()
+        i += 1
+
 
 # computes every state which is reachable on epsilon transition from the provided set of states
 # essentially a DFS algorithm
@@ -48,17 +64,75 @@ def move(states, sym, nfa):
     return move_set
 
 
+# return the set of input symbols (excluding epsilon) for which the provided NFA states have outgoing transitions
+def compute_possible_in_syms(states, nfa):
+    syms = set()
+    for state_index in states:
+        state = nfa[state_index]
+        for in_sym in state:
+            if in_sym[0] != 'eps':
+                syms.add(in_sym[0])
+    return syms
+
+
+# perform NFA to DFA conversion using the subset creation algorithm
+# it requires an NFA transition matrix and a list of (regex pattern, accepting state of the NFA for that pattern) pairs
+# as input
+# returns the set of new DFA states, DFA transition matrix, and the list of accepting DFA states
+def nfa_to_dfa(nfa, pattern_acc_states):
+    # list of DFA states
+    dstates = [eps_closure([0], nfa)]
+    # additional list which contains 'marked' flags for each DFA state
+    marked = [False]
+    # DFA transition matrix
+    dtran = []
+    # list of accepting DFA states
+    acc_states = []
+    while True:
+        curr_index = 0
+        curr_state = None
+        # check whether there are unmarked states in the list
+        if marked.count(False) > 0:
+            curr_index = marked.index(False)
+            curr_state = dstates[curr_index]
+            marked[curr_index] = True
+        else:
+            break
+
+        dtran.append([])
+        input_syms = compute_possible_in_syms(curr_state, nfa)
+        for sym in input_syms:
+            new_state = eps_closure(move(curr_state, sym, nfa), nfa)
+            if dstates.count(new_state) == 0:
+                dstates.append(new_state)
+                marked.append(False)
+                # if at least one NFA state from the set of NFA states that represent this DFA state is an accepting
+                # state, then this DFA state should be marked as accepting too
+                for nfa_state in new_state:
+                    if nfa_state in pattern_acc_states.keys():
+                        acc_states.append(dstates.index(new_state))
+            dtran[curr_index].append([sym, dstates.index(new_state)])
+
+    return dstates, dtran, acc_states
+
+
 if __name__ == "__main__":
     token_list_1 = tokenize_regex('a')
-    token_list_2 = tokenize_regex('b')
+    token_list_2 = tokenize_regex('abb')
+    token_list_3 = tokenize_regex('a*b')
     root_1 = parse(token_list_1)
     root_2 = parse(token_list_2)
+    root_3 = parse(token_list_3)
     mat_1 = regex_to_nfa(root_1)
     print_matrix(mat_1)
     mat_2 = regex_to_nfa(root_2)
     print_matrix(mat_2)
-    (mat, acc_states) = combine_nfas([mat_1, mat_2])
-    print(mat)
+    mat_3 = [[['a', 0], ['b', 1], ['eps']], [['b', 1], ['eps']]]
+    print_matrix(mat_3)
+    pattern_dict = [('pat1', mat_1), ('pat2', mat_2), ('pat3', mat_3)]
 
-    print(eps_closure([4], mat))
-    print(move([1], 'a', mat))
+    (nfa, pattern_acc_states) = combine_nfas(pattern_dict)
+    print_matrix(nfa)
+
+    (dstates, dtran, dfa_acc_states) = nfa_to_dfa(nfa, pattern_acc_states)
+    print_dfa(dstates, dtran, dfa_acc_states)
