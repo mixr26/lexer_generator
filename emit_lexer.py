ffd56496911c63c7a662fd1477b6092944b9561f
mixr26 <mihailo.stojanovic26@gmail.com>
2020-05-02 15:40:01 +0200

    Finish implementation of lexer code generation
            * emit_lexer.py (create_header_and_emit_manifest): Add includes.
            Add token type defines (error and default). Add token_type and
            ignore fields to Token class. Reverse the removal of error state
            from the States enum class. Make next_word method private, and
            change its return type to a pointer to Token. Make Lexer class
            constructor open an input file (and destructor close it). Add
            new member method - get_next_word, a wrapper for next_word.
            (create_body): Emit user-provided code as functions for each
            pattern. Emit the get_next_word Lexer method. Emit calls to
            user-provided calls.

--- emit_lexer.py
+++ emit_lexer.py
@@ -1,3 +1,4 @@
+from pattern_descriptor import PatternDesc
 from regex_parser import parse
 from regex_lexer import tokenize_regex
 from regex_to_nfa import combine_nfas, regex_to_nfa, print_matrix
@@ -24,7 +25,11 @@ def create_header_and_emit_manifest(manifest, states_num):
             "#include <string>\n"
             "#include <fstream>\n"
             "#include <stack>\n"
+            "#include <memory>\n"
+            "#include <functional>\n"
             "#include <cstdint>\n\n"
+            "#define TOKEN_TYPE_ERROR 0\n"
+            "#define TOKEN_TYPE_DEFAULT 1\n\n"
         ])
 
         # emit manifest code
@@ -34,20 +39,28 @@ def create_header_and_emit_manifest(manifest, states_num):
         # emit Token class
         header.writelines([
             "class Token {\n"
+            "\tuint16_t token_type;\n"
             "\tstd::string lexeme;\n"
             "\t// Is this the last token of the stream.\n"
             "\tbool last;\n"
             "\t// Line in input file which contains this lexeme. Currently only used for\n"
             "\t// error reporting.\n"
             "\tint16_t line;\n"
+            "\t// Whether this token should be ignored.\n"
+            "\tbool ignore;\n"
             "public:\n"
             "\tToken() = delete;\n"
-            "\tToken(std::string lexeme, int16_t line = 0)\n"
-            "\t\t: lexeme(lexeme), last(false), line(line) {}\n\n"
+            "\tToken(std::string lexeme, uint16_t token_type = TOKEN_TYPE_DEFAULT, bool last = false, int16_t line = "
+            "0, bool ignore = false)\n "
+            "\t\t: lexeme(lexeme), token_type(token_type), last(last), line(line), ignore(ignore) {}\n\n"
+            "\tvoid set_token_type(uint16_t token_type) { this->token_type = token_type; }\n"
+            "\tvoid set_ignore(bool ignore) { this->ignore = ignore; }\n"
             "\tvoid set_lexeme(std::string lexeme) { this->lexeme = lexeme; }\n"
             "\tvoid set_last(bool last) { this->last = last; }\n\n"
             "\tstd::string& get_lexeme() { return this->lexeme; }\n"
             "\tbool is_last() { return this->last; }\n"
+            "\tuint16_t get_token_type() { return this->token_type; }\n"
+            "\tbool is_ignore() { return this->ignore; }\n"
             "\tint16_t get_line() { return this->line; }\n"
             "};\n\n"
         ])
@@ -58,12 +71,13 @@ def create_header_and_emit_manifest(manifest, states_num):
         header.write("\tS0 = 0,\n")
         for i in range (1, states_num):
             header.write("\tS" + str(i) + ",\n")
-        header.write("\tBAD,\n")
+        header.write("\tSE,\n")
+        header.write("\tBAD\n")
         header.write("};\n\n")
 
         # emit Lexer class
         header.write("// Takes a stream of characters from the input file and tokenizes them\n")
-        header.write("into a stream of lexemes.\n\n")
+        header.write("// into a stream of lexemes.\n\n")
 
         header.writelines([
             "class Lexer {\n"
@@ -74,7 +88,7 @@ def create_header_and_emit_manifest(manifest, states_num):
             "\t// Current DFA state.\n"
             "\tStates state;\n"
             "\t// A stack of states used for backtracking.\n"
-            "\tstd::stack<States> states_stack;\n\n"
+            "\tstd::stack<States> states_stack;\n"
             "\t// Return the next character of the input file.\n"
             "\tchar next_char() { return static_cast<char>(this->filestream.get()); }\n"
             "\t// Roll back the filestream one character.\n"
@@ -83,18 +97,19 @@ def create_header_and_emit_manifest(manifest, states_num):
             "\tstatic constexpr bool is_accepting_state(States);\n"
             "\t// Whether the provided character is newline.\n"
             "\tstatic constexpr bool is_newline(char c) { return c == 10 || c == 13; }\n"
+            "\t// Try to tokenize next word from the input file. This is the heart of the\n"
+            "\t// lexer. This method implements a table-driven, direct-coded scanning algorithm\n"
+            "\t// described in 'Engineering a Compiler' by Cooper and Torczon (2nd edition, p. 60).\n"
+            "\tstd::shared_ptr<Token> next_word();\n"
             "public:\n"
             "\tLexer() = delete;\n"
             "\tLexer(const Lexer&) = delete;\n"
             "\tLexer(Lexer&&) = delete;\n"
-            "\tLexer() {}\n"
-            "\t~Lexer() {}\n"
+            "\tLexer(const char* input_file) { this->filestream.open(input_file); }\n"
+            "\t~Lexer() { this->filestream.close(); }\n"
             "\tLexer& operator=(Lexer&) = delete;\n"
-            "\tLexer& operator=(Lexer&&) = delete;\n\n"
-            "\t// Try to tokenize next word from the input file. This is the heart of the\n"
-            "\t// lexer. This method implements a table-driven, direct-coded scanning algorithm\n"
-            "\t// described in 'Engineering a Compiler' by Cooper and Torczon (2nd edition, p. 60).\n"
-            "\tuint32_t next_word();\n"
+            "\tLexer& operator=(Lexer&&) = delete;\n"
+            "\tstd::shared_ptr<Token> get_next_word();\n"
             "};\n\n"
         ])
 
@@ -108,8 +123,15 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
         # emit includes
         body.writelines([
             "#include <type_traits>\n"
-            "#include \"lexer.h\"\n\n"
+            "#include \"my_little_lexer.h\"\n\n"
         ])
+
+        # emit the functions which contain the user-provided code for each pattern
+        for patt_desc in pattern_descs:
+            if patt_desc.code != '':
+                body.write("void " + patt_desc.name + "(std::shared_ptr<Token> token)\n")
+                body.write(patt_desc.code)
+                body.write("\n\n")
 
         # emit the is_accepting_state Lexer class method
         body.writelines([
@@ -119,17 +141,28 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
         ])
 
         for state in dfa_acc_states:
-            body.write("\t\t\t|| s == S" + str(state) + "\n")
+            body.write("\t\t\t|| s == States::S" + str(state) + "\n")
         body.write("\t\t\t);\n")
 
         body.write("}\n\n")
 
+        # emit the get_next_word Lexer method
+        body.writelines([
+            "std::shared_ptr<Token> Lexer::get_next_word() {\n"
+            "\tstd::shared_ptr<Token> tok;\n"
+            "\twhile((tok = this->next_word())->is_ignore());\n"
+            "\treturn tok;\n"
+            "}\n\n"
+        ])
+
         # emit the next_word Lexer method
         body.writelines([
-            "uint32_t Lexer::next_word() {\n"
+            "std::shared_ptr<Token> Lexer::next_word() {\n"
             "Init:\n"
+            "\tbool last{false};\n"
             "\tstatic int16_t line{1};\n"
             "\tstd::string lexeme{""};\n"
+            "\tchar c;\n"
             "\tthis->state = States::S0;\n\n"
             "\twhile (!this->states_stack.empty())\n"
             "\t\tthis->states_stack.pop();\n"
@@ -140,11 +173,13 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
             body.writelines([
                 "S" + str(i) + ":\n"
                 "\tthis->state = States::S" + str(i) + ";\n"
-                "\tchar c{this->next_char()};\n"
-                "\tif (c == std::char_traits<char>::eof())\n"
-                "\t\tgoto SOut;\n\n"
+                "\tc = this->next_char();\n"
+                "\tif (c == std::char_traits<char>::eof()) {\n"
+                "\t\tlast = true;\n"
+                "\t\tgoto SOut;\n"
+                "\t}\n\n"
                 "\tif (this->is_newline(c))\n"
-                "\t\tthis->line++;\n\n"
+                "\t\tline++;\n\n"
                 "\tlexeme.push_back(c);\n\n"
                 "\tif (this->is_accepting_state(this->state))\n"
                 "\t\twhile (!this->states_stack.empty())\n"
@@ -156,10 +191,12 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
             state = dtran[i]
             for in_sym in state:
                 body.writelines([
-                    "\tcase " + "\'" + in_sym[0] + "\':\n"
+                    "\tcase " + repr(in_sym[0]) + ":\n"
                     "\t\tgoto S" + str(in_sym[1]) + ";\n"
                 ])
-            body.write("\tdefault:\n\t\tgoto SOut;\n")
+            body.writelines(["\tdefault:\n",
+                             "\t\tthis->state = States::SE;\n"
+                             "\t\tgoto SOut;\n"])
 
             body.write("\t}\n\n")
 
@@ -174,26 +211,48 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
             "\t\t\tlexeme.pop_back();\n"
             "\t\tthis->rollback();\n"
             "\t}\n\n"
+            "\tstd::shared_ptr<Token> tok{std::make_shared<Token>(lexeme, TOKEN_TYPE_DEFAULT, last, line, false)};\n"
+            "\tif (this->is_accepting_state(this->state)) {\n"
+        ])
+
+        # each DFA accepting states recognize exactly one pattern, so just iterate through the list of patterns
+        # and emit calls to its user-provided code
+        body.write("\t\tswitch(this->state) {\n")
+        for patt_desc in pattern_descs:
+            for dfa_acc_state in patt_desc.dfa_acc_states:
+                body.write("\t\tcase States::S" + str(dfa_acc_state) + ":\n")
+                body.write("\t\t\t" + patt_desc.name + "(tok);\n")
+                body.write("\t\t\tbreak;\n")
+        body.write("\t\t}\n")
+
+        body.writelines([
+            "\t}\n"
+            "\telse\n"
+            "\t\ttok->set_token_type(TOKEN_TYPE_ERROR);\n\n"
+            "\treturn tok;\n"
         ])
 
         body.write("}\n\n")
 
 
 if __name__ == "__main__":
-    token_list_1 = tokenize_regex("(a|b)*abb")
-    root_1 = parse(token_list_1)
+    mat_1 = regex_to_nfa(parse(tokenize_regex("_[a-z][a-z]*")))
+    mat_2 = regex_to_nfa(parse(tokenize_regex("[1-9][1-9]*")))
+    mat_3 = regex_to_nfa(parse(tokenize_regex("( |\n)( |\n)*")))
 
-    mat_1 = regex_to_nfa(root_1)
+    pattern_descs = [PatternDesc("id_pat", "{ token->set_token_type(TOKEN_TYPE_ID); }", mat_1),
+                     PatternDesc("num_pat", "{ token->set_token_type(TOKEN_TYPE_NUM); }", mat_2),
+                     PatternDesc("ws_pat", "{ token->set_token_type(TOKEN_TYPE_WS); token->set_ignore(true); }", mat_3)]
 
-    pattern_dict = [("pat1", mat_1)]
+    (nfa, pattern_descs) = combine_nfas(pattern_descs)
 
-    (nfa, nfa_acc_states) = combine_nfas(pattern_dict)
-    print(nfa_acc_states)
-
-    #(dstates, dtran, dfa_acc_states) = nfa_to_dfa(nfa, nfa_acc_states)
+    (dstates, dtran, dfa_acc_states, pattern_descs) = nfa_to_dfa(nfa, pattern_descs)
     #print_matrix(dtran)
     #print_dfa(dstates, dtran, dfa_acc_states)
     #print(dfa_acc_states)
+    #for pattern_desc in pattern_descs:
+        #print(pattern_desc)
 
-    #create_header_and_emit_manifest([""], len(dstates))
-    #create_body(dstates, dtran, dfa_acc_states, nfa_acc_states)
+    create_header_and_emit_manifest(["#define TOKEN_TYPE_WS 2\n", "#define TOKEN_TYPE_ID 3\n",
+                                     "#define TOKEN_TYPE_NUM 4\n"], len(dstates))
+    create_body(dstates, dtran, dfa_acc_states, pattern_descs)
