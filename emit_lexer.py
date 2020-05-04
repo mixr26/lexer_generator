a90532967cba0ca5aad6cc05c74cf84bb68ffcb1
Mihailo Stojanovic <mihailo.stojanovic@rt-rk.com>
2020-05-04 14:26:50 +0200

    Make the last token of the stream have TOKEN_TYPE_LAST
    Until now, last token in the stream had had its "last" field set.
    Since now, there will always be a token with the type TOKEN_TYPE_LAST at
    the end of the token stream.
    
            * emit_lexer.py (create_header_and_manifest): Add
            TOKEN_TYPE_LAST define. Remove "last" field from Token class.
            Revert the ban of the Token class empty constructor.
            (create_body): Remove the jump to SOut on EOF. Create and return
            a new token if EOF is the only character in the lexeme.

--- emit_lexer.py
+++ emit_lexer.py
@@ -29,7 +29,8 @@ def create_header_and_emit_manifest(manifest, states_num):
             "#include <functional>\n"
             "#include <cstdint>\n\n"
             "#define TOKEN_TYPE_ERROR 0\n"
-            "#define TOKEN_TYPE_DEFAULT 1\n\n"
+            "#define TOKEN_TYPE_LAST 1\n"
+            "#define TOKEN_TYPE_DEFAULT 2\n\n"
         ])
 
         # emit manifest code
@@ -41,23 +42,20 @@ def create_header_and_emit_manifest(manifest, states_num):
             "class Token {\n"
             "\tuint16_t token_type;\n"
             "\tstd::string lexeme;\n"
-            "\t// Is this the last token of the stream.\n"
-            "\tbool last;\n"
             "\t// Line in input file which contains this lexeme. Currently only used for\n"
             "\t// error reporting.\n"
             "\tint16_t line;\n"
             "\t// Whether this token should be ignored.\n"
             "\tbool ignore;\n"
             "public:\n"
-            "\tToken(std::string lexeme = \"\", uint16_t token_type = TOKEN_TYPE_DEFAULT, bool last = false, int16_t line = "
+            "\tToken() = delete;\n"
+            "\tToken(std::string lexeme = \"\", uint16_t token_type = TOKEN_TYPE_DEFAULT, int16_t line = "
             "0, bool ignore = false)\n "
-            "\t\t: lexeme(lexeme), token_type(token_type), last(last), line(line), ignore(ignore) {}\n\n"
+            "\t\t: lexeme(lexeme), token_type(token_type), line(line), ignore(ignore) {}\n\n"
             "\tvoid set_token_type(uint16_t token_type) { this->token_type = token_type; }\n"
             "\tvoid set_ignore(bool ignore) { this->ignore = ignore; }\n"
-            "\tvoid set_lexeme(std::string lexeme) { this->lexeme = lexeme; }\n"
-            "\tvoid set_last(bool last) { this->last = last; }\n\n"
+            "\tvoid set_lexeme(std::string lexeme) { this->lexeme = lexeme; }\n\n"
             "\tconst std::string& get_lexeme() const { return this->lexeme; }\n"
-            "\tbool is_last() const { return this->last; }\n"
             "\tuint16_t get_token_type() const { return this->token_type; }\n"
             "\tbool is_ignore() const { return this->ignore; }\n"
             "\tint16_t get_line() const { return this->line; }\n"
@@ -144,7 +142,7 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
             "\tos << \"Token type: \" << tok.get_token_type() << std::endl;\n"
             "\tos << \"Lexeme: \" << tok.get_lexeme() << std::endl;\n"
             "\tos << \"Line: \" << tok.get_line() << std::endl;\n"
-            "\tif (tok.is_last())\n"
+            "\tif (tok.get_token_type() == TOKEN_TYPE_LAST)\n"
             "\t\tos << \"Last\" << std::endl;\n\n"
             "\treturn os;\n"
             "}\n\n"
@@ -173,7 +171,7 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
         # emit the get_next_word Lexer method
         body.writelines([
             "std::shared_ptr<Token> Lexer::get_next_word() {\n"
-            "\tstd::shared_ptr<Token> tok = std::make_shared<Token>();\n"
+            "\tstd::shared_ptr<Token> tok;\n"
             "\twhile((tok = this->next_word())->is_ignore());\n"
             "\treturn tok;\n"
             "}\n\n"
@@ -183,7 +181,6 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
         body.writelines([
             "std::shared_ptr<Token> Lexer::next_word() {\n"
             "Init:\n"
-            "\tbool last{false};\n"
             "\tstatic int16_t line{1};\n"
             "\tstd::string lexeme{""};\n"
             "\tchar c;\n"
@@ -196,12 +193,8 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
         for i in range(len(dstates)):
             body.writelines([
                 "S" + str(i) + ":\n"
-                "\tthis->state = States::S" + str(i) + ";\n"
+                "\tthis->state = States::S" + str(i) + ";\n\n"
                 "\tc = this->next_char();\n"
-                "\tif (c == std::char_traits<char>::eof()) {\n"
-                "\t\tlast = true;\n"
-                "\t\tgoto SOut;\n"
-                "\t}\n\n"
                 "\tif (this->is_newline(c))\n"
                 "\t\tline++;\n\n"
                 "\tlexeme.push_back(c);\n\n"
@@ -235,7 +228,7 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
             "\t\t\tlexeme.pop_back();\n"
             "\t\tthis->rollback();\n"
             "\t}\n\n"
-            "\tstd::shared_ptr<Token> tok{std::make_shared<Token>(lexeme, TOKEN_TYPE_DEFAULT, last, line, false)};\n"
+            "\tstd::shared_ptr<Token> tok{std::make_shared<Token>(lexeme, TOKEN_TYPE_DEFAULT, line, false)};\n"
             "\tif (this->is_accepting_state(this->state)) {\n"
         ])
 
@@ -251,6 +244,8 @@ def create_body(dstates, dtran, dfa_acc_states, nfa_acc_states):
 
         body.writelines([
             "\t}\n"
+            "\telse if (c == std::char_traits<char>::eof())\n"
+            "\t\ttok->set_token_type(TOKEN_TYPE_LAST);\n"
             "\telse\n"
             "\t\ttok->set_token_type(TOKEN_TYPE_ERROR);\n\n"
             "\treturn tok;\n"
@@ -266,7 +261,7 @@ if __name__ == "__main__":
 
     pattern_descs = [PatternDesc("id_pat", "{ token->set_token_type(TOKEN_TYPE_ID); }", mat_1),
                      PatternDesc("num_pat", "{ token->set_token_type(TOKEN_TYPE_NUM); }", mat_2),
-                     PatternDesc("ws_pat", "{ token->set_token_type(TOKEN_TYPE_WS); token->set_ignore(true); }", mat_3)]
+                     PatternDesc("ws_pat", "{ token->set_token_type(TOKEN_TYPE_WS); /*token->set_ignore(true);*/ }", mat_3)]
 
     (nfa, pattern_descs) = combine_nfas(pattern_descs)
 
@@ -277,6 +272,6 @@ if __name__ == "__main__":
     #for pattern_desc in pattern_descs:
         #print(pattern_desc)
 
-    create_header_and_emit_manifest(["#define TOKEN_TYPE_WS 2\n", "#define TOKEN_TYPE_ID 3\n",
-                                     "#define TOKEN_TYPE_NUM 4\n"], len(dstates))
+    create_header_and_emit_manifest(["#define TOKEN_TYPE_WS 3\n", "#define TOKEN_TYPE_ID 4\n",
+                                     "#define TOKEN_TYPE_NUM 5\n"], len(dstates))
     create_body(dstates, dtran, dfa_acc_states, pattern_descs)
